b43014ef7d39c58c0ea8a7d6e13f4c622a97135c
bozont <884528-bozont91@users.noreply.gitlab.com>
2026-01-18 16:30:39 +0100

    Use unsigned constants consistently

--- src/bozontlabsMAX7219.cpp
+++ src/bozontlabsMAX7219.cpp
@@ -6,8 +6,8 @@ bozontlabsMAX7219::bozontlabsMAX7219(pin_size_t pin_CS, pin_size_t pin_MOSI, pin
   this->display_pin_CLK = pin_CLK;
   this->display_pin_CS = pin_CS;
 
-  if (devices <= 0 || devices > 8) {
-    devices = 8;
+  if (devices <= 0u || devices > 8u) {
+    devices = 8u;
   }
   this->num_devices = devices;
   this->display_height = 8u;
@@ -18,8 +18,8 @@ bozontlabsMAX7219::bozontlabsMAX7219(pin_size_t pin_CS, pin_size_t pin_MOSI, pin
 bozontlabsMAX7219::bozontlabsMAX7219(pin_size_t pin_CS, uint8_t devices, SPIClass* spi_bus, uint32_t spi_bus_speed) {
   this->display_pin_CS = pin_CS;
 
-  if (devices <= 0 || devices > 8) {
-    devices = 8;
+  if (devices <= 0u || devices > 8u) {
+    devices = 8u;
   }
   this->num_devices = devices;
   this->display_height = 8u;
@@ -43,9 +43,9 @@ void bozontlabsMAX7219::begin() {
 
   memset(this->pixel_states, 0u, sizeof(this->pixel_states));
 
-  for (uint8_t i = 0; i < this->num_devices; i++) {
-    this->spiTransfer(i, REG_DISPLAYTEST, 0);
-    this->spiTransfer(i, REG_DECODEMODE, 0);
+  for (uint8_t i = 0u; i < this->num_devices; i++) {
+    this->spiTransfer(i, REG_DISPLAYTEST, 0u);
+    this->spiTransfer(i, REG_DECODEMODE, 0u);
   }
   this->clearDisplay();
   this->setScanLimit(this->led_matrix_scan_limit);
@@ -68,9 +68,9 @@ uint8_t bozontlabsMAX7219::getDisplayHeight() {
 void bozontlabsMAX7219::shutdown(bool shutdown) {
   uint8_t cmd_payload;
   if (!shutdown) {
-    cmd_payload = 0x01;
+    cmd_payload = 1u;
   } else {
-    cmd_payload = 0x00;
+    cmd_payload = 0u;
   }
   for (uint8_t addr = 0u; addr < this->num_devices; addr++) {
     this->spiTransfer(addr, REG_SHUTDOWN, cmd_payload);
@@ -93,10 +93,10 @@ void bozontlabsMAX7219::setBrightness(uint8_t brightness) {
 
 void bozontlabsMAX7219::clearDisplay() {
   for (uint8_t addr = 0u; addr < this->num_devices; addr++) {
-    uint8_t offset = addr * 8;
-    for (uint8_t i = 0; i < 8; i++) {
-      this->pixel_states[offset + i] = 0;
-      this->spiTransfer(addr, i + 1, this->pixel_states[offset + i]);
+    uint8_t offset = addr * 8u;
+    for (uint8_t i = 0u; i < 8u; i++) {
+      this->pixel_states[offset + i] = 0u;
+      this->spiTransfer(addr, i + 1u, this->pixel_states[offset + i]);
     }
   }
 }
@@ -105,51 +105,49 @@ void bozontlabsMAX7219::setPixel(uint8_t x, uint8_t y, bool value) {
   if (x >= this->display_width || y >= this->display_height) {
     return;
   }
-  uint8_t module_number = x / 8;
-  uint8_t x_inside_module = x % 8;
-  this->setLed(module_number, 7 - y, 7 - x_inside_module, value);
+  uint8_t module_number = x / 8u;
+  uint8_t x_inside_module = x % 8u;
+  this->setLed(module_number, 7u - y, 7u - x_inside_module, value);
 }
 
 void bozontlabsMAX7219::setLed(uint8_t addr, uint8_t row, uint8_t column, bool state) {
-  uint8_t offset;
-  uint8_t val = 0x00;
   if (addr < 0u || addr >= this->num_devices) {
     return;
   }
   if (row < 0u || row > 7u || column < 0u || column > 7u) {
     return;
   }
-  offset = addr * 8u;
-  val = B10000000 >> column;
+  uint8_t offset = addr * 8u;
+  uint8_t val = B10000000 >> column;
   if (state) {
     this->pixel_states[offset + row] = this->pixel_states[offset + row] | val;
   } else {
     val = ~val;
     this->pixel_states[offset + row] = this->pixel_states[offset + row] & val;
   }
-  this->spiTransfer(addr, row + 1, this->pixel_states[offset + row]);
+  this->spiTransfer(addr, row + 1u, this->pixel_states[offset + row]);
 }
 
 void bozontlabsMAX7219::spiTransfer(uint8_t addr, uint8_t reg, uint8_t data) {
   static uint8_t spi_output_buffer[16];
-  uint8_t offset = addr * 2;
-  uint8_t maxbytes = this->num_devices * 2;
+  uint8_t offset = addr * 2u;
+  uint8_t maxbytes = this->num_devices * 2u;
 
   memset(spi_output_buffer, 0u, maxbytes);
 
   spi_output_buffer[offset] = data;
-  spi_output_buffer[offset + 1] = reg;
+  spi_output_buffer[offset + 1u] = reg;
 
   digitalWrite(this->display_pin_CS, LOW);
 
   if (this->spi_bus_mode == BUSMODE_SOFTWARE_SPI) {
-    for (uint8_t i = maxbytes; i > 0; i--) {
-      shiftOut(this->display_pin_MOSI, this->display_pin_CLK, MSBFIRST, spi_output_buffer[i - 1]);
+    for (uint8_t i = maxbytes; i > 0u; i--) {
+      shiftOut(this->display_pin_MOSI, this->display_pin_CLK, MSBFIRST, spi_output_buffer[i - 1u]);
     }
   } else if (this->spi_bus_mode == BUSMODE_HARDWARE_SPI) {
     this->hw_spi_bus->beginTransaction(SPISettings(this->spi_bus_speed, MSBFIRST, SPI_MODE0));
-    for (uint8_t i = maxbytes; i > 0; i--) {
-      this->hw_spi_bus->transfer(spi_output_buffer[i - 1]);
+    for (uint8_t i = maxbytes; i > 0u; i--) {
+      this->hw_spi_bus->transfer(spi_output_buffer[i - 1u]);
     }
     this->hw_spi_bus->endTransaction();
   }
